fe14e1aafe7dac75ca0552278b3a1cc3ebe07cf8
TheWhiteMachine <priveiro@gmail.com>
2023-12-19 16:07:03 -0800

    fixing eslint issues

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,15 +1,13 @@
 process.stdin.setEncoding('utf8');
 
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-process.stdin.on('readable', function () {
-	let input = process.stdin.read();
-	if (input !== null) {
-		process.stdout.write('Your name is: ' + input);
-	}
+process.stdin.on('readable', () => {
+  const input = process.stdin.read();
+  if (input !== null) {
+    process.stdout.write('Your name is: ' + input);
+  }
 });
 process.stdin.end();
-process.stdin.on('end', function () {
-	process.stdout.write('This important software is now closing\n');
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
-
-
