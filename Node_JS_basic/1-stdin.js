1ec51dad5239eaa1b304882909ce31a3d9250d92
TheWhiteMachine <priveiro@gmail.com>
2023-12-19 16:09:28 -0800

    eslint thing

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -4,7 +4,7 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', () => {
   const input = process.stdin.read();
   if (input !== null) {
-    process.stdout.write('Your name is: ' + input);
+    process.stdout.write(`Your name is:  ${input}`);
   }
 });
 process.stdin.end();
